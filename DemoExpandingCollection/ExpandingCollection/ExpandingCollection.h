aec1fba654e797205d1cad0f8cc1abf5f5b42a66
Alex.k <Alex.k@ramotion.com>
2016-06-02 10:54:40 +0300

    shared scheme

---
+++ DemoExpandingCollection/ExpandingCollection/ExpandingCollection.h
@@ -0,0 +1,19 @@
+//
+//  ExpandingCollection.h
+//  ExpandingCollection
+//
+//  Created by Alex K. on 02/06/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExpandingCollection.
+FOUNDATION_EXPORT double ExpandingCollectionVersionNumber;
+
+//! Project version string for ExpandingCollection.
+FOUNDATION_EXPORT const unsigned char ExpandingCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExpandingCollection/PublicHeader.h>
+
+
